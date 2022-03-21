fabe7b8d057acd3e9394301bfc1f487a74b59579
martproz <martinprozapas@gmail.com>
2022-03-20 22:47:37 -0300

    Proyecto Freecode React 20 03 22

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
+
+//Para el clon de Freecode testimonios, necesitaremos
+//un contenedor principal, una imagen y una caja con texto
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+        <img className='imagen-testimonio' 
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt= 'Foto de Emma' />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+          </p>
+        <p className='cargo-testimonio'>
+        {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"</p>
+      </div>
+  </div>
+  );
+
+}
+
+export default Testimonio;
