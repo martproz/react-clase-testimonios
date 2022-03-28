6fdcb812bcc825325d5873e290e4be1379f8fa42
martproz <martinprozapas@gmail.com>
2022-03-28 02:00:28 -0300

    beta

--- src/App.js
+++ src/App.js
@@ -31,9 +31,8 @@ class App extends React.Component {
           testimonio= 'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' />
         </div>
       </div>
-    );
+    ); 
   }
 }
-
 
 export default App;
