97fcd5b1e6453eb5dc021ab935a4261a7d08bdcf
martproz <martinprozapas@gmail.com>
2022-03-28 01:59:04 -0300

    28 03 22

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css'
 
-//Para el clon de Freecode testimonios, necesitaremos
-//un contenedor principal, una imagen y una caja con texto
-function Testimonio(props) {
-  return (
-    <div className='contenedor-testimonio'>
-        <img className='imagen-testimonio' 
-        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-        alt= 'Foto de Emma' />
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>
-          <strong>{props.nombre}</strong> en {props.pais}
-          </p>
-        <p className='cargo-testimonio'>
-        {props.cargo} en <strong>{props.empresa}</strong>
-        </p>
-        <p className='texto-testimonio'>
-          "{props.testimonio}"</p>
-      </div>
-  </div>
-  );
 
+class Testimonio extends React.Component {
+  render() {
+    return (
+      <div className='contenedor-testimonio'>
+          <img className='imagen-testimonio' 
+          src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
+          alt={`Foto de ${this.props.nombre}`} />
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>
+            <strong>{this.props.nombre}</strong> en {this.props.pais}
+            </p>
+          <p className='cargo-testimonio'>
+          {this.props.cargo} en <strong>{this.props.empresa}</strong>
+          </p>
+          <p className='texto-testimonio'>
+            "{this.props.testimonio}"</p>
+        </div>
+    </div>
+    );
+  }
 }
+
 
 export default Testimonio;
